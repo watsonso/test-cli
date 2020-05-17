d057ffc0ef80225a7bd1b9a3cebd31e2625973e7
watsonso <uni0360@gmail.com>
2020-05-17 09:39:30 +0900

    Fix

--- main.go
+++ main.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
+	// flagパッケージについて ref: https://golang.org/pkg/flag
+	// flag.String("引数の名前", "デフォルト値", "ヘルプメッセージ")
 	var name = flag.String("name", "Aoki", "please specify -name flag")
 	flag.Parse()
+
+	// fmtパッケージについて ref: https://golang.org/pkg/fmt/
 	fmt.Println("This name flag is", *name)
 }
